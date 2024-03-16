19f882187a0caae271e03e2680706f8648ea844d
christiannaira <christianmnaira@gmail.com>
2024-03-16 12:04:47 +0800

    feat(components & router): Added new components and pages routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import { ChannelDetail, VideoDetail, SearchFeed, Navbar, Feed } from "./components";
 
 function App() {
 
   return (
     <>
-      hello
+      <BrowserRouter>
+
+        <Box sx={{ backgroundColor: '#000' }}>
+
+          <Navbar />
+
+          <Routes>
+
+            <Route path="/" element={<Feed />} />
+            <Route path="/video/:id" element={<VideoDetail />} />
+            <Route path="/channel/:id" element={<ChannelDetail />} />
+            <Route path="/search/:searchTerm" element={<SearchFeed />} />
+
+          </Routes>
+
+        </Box>
+
+      </BrowserRouter>
     </>
   )
 }

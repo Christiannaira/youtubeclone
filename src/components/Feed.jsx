2cd01d722f0d8035afbd3bc88d98c40422257f2d
christiannaira <christianmnaira@gmail.com>
2024-03-16 15:34:24 +0800

    feat(fetching): Added fetchi API functionality

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,15 +1,21 @@
 
+import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
 
 import { Videos, Sidebar } from "./";
 
 const Feed = () => {
+
+    const [selectedCategory, setSelectedCategory] = useState("New");
+    const [videos, setVideos] = useState(null);
+
+
     return (
         <Stack sx={{ flexDirection: { sx: 'column', md: "row" } }}>
 
             <Box sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 } }}>
 
-                {/* <Sidebar /> */}
+                <Sidebar />
 
             </Box>
 
--- src/utils/fetchFromApi.js
+++ src/utils/fetchFromApi.js
@@ -3,12 +3,21 @@ import axios from "axios";
 export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
 const options = {
-    url: BASE_URL,
+    // url: BASE_URL,
     params: {
         maxResults: '50'
     },
     headers: {
         'X-RapidAPI-Key': import.meta.env.REACT_APP_RAPID_API_KEY,
-        'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+        'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
     }
 };
+
+export const fetchFromAPi = async (url) => {
+
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+    return data;
+
+}
+

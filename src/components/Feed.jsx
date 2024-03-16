dd252591f1ff82ffab7a047e90086b5bf0478867
christiannaira <christianmnaira@gmail.com>
2024-03-16 15:28:54 +0800

    feat(feed): Developed the initial design of feed component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+
+import { Box, Stack, Typography } from "@mui/material";
+
+import { Videos, Sidebar } from "./";
 
 const Feed = () => {
     return (
-        <div>
-            Feed
-        </div>
+        <Stack sx={{ flexDirection: { sx: 'column', md: "row" } }}>
+
+            <Box sx={{ height: { sx: 'auto', md: '92vh' }, borderRight: '1px solid #3d3d3d', px: { sx: 0, md: 2 } }}>
+
+                {/* <Sidebar /> */}
+
+            </Box>
+
+        </Stack>
     )
 }
 
---
+++ src/utils/fetchFromApi.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+    url: BASE_URL,
+    params: {
+        maxResults: '50'
+    },
+    headers: {
+        'X-RapidAPI-Key': import.meta.env.REACT_APP_RAPID_API_KEY,
+        'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+    }
+};

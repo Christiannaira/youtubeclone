5b091502406323a90be3e76d6dae3c4bbc62ee96
christiannaira <christianmnaira@gmail.com>
2024-03-18 21:22:20 +0800

    fix(api): Initially removed .env file to check if fetching api is working

--- src/utils/fetchFromApi.js
+++ src/utils/fetchFromApi.js
@@ -3,12 +3,12 @@ import axios from "axios";
 export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
 const options = {
-    // url: BASE_URL,
+
     params: {
         maxResults: '50'
     },
     headers: {
-        'X-RapidAPI-Key': import.meta.env.REACT_APP_RAPID_API_KEY,
+        'X-RapidAPI-Key': '1e8571aaf4msh6c0ad5a8f8c2affp1e9d7ejsnc63beb1b3a52',
         'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
     }
 };
@@ -17,7 +17,7 @@ export const fetchFromAPi = async (url) => {
 
     const { data } = await axios.get(`${BASE_URL}/${url}`, options);
 
-    return data;
+    console.log(data);
 
 }
 

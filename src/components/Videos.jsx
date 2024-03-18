f43871aa5d071676c5e1cf8081f22cb7fe1b458c
christiannaira <christianmnaira@gmail.com>
2024-03-18 23:05:34 +0800

    feat(videocard): Developed VideoCard contents

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -2,12 +2,12 @@ import { Stack, Box } from "@mui/material";
 
 import { ChannelCard, Loader, VideoCard } from "./";
 
-const Videos = ({ videos }) => {
+const Videos = ({ videos, direction }) => {
 
     if (!videos?.length) return <Loader />
 
     return (
-        <Stack>
+        <Stack direction={direction || 'row'} flexWrap="wrap" justifyContent="start" gap={2}>
 
             {videos.map((item, idx) => (
 
